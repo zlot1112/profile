e5edf3e9e99d10919d4172715a6a19a10b5bfe13
LeeJinKyu <zlot1112@gmail.com>
2022-03-15 23:06:24 +0900

    pdf down

--- src/build.js
+++ src/build.js
@@ -3,7 +3,6 @@ const fs = require('fs-extra');
 const markdownHelper = require('./utils/helpers/markdown');
 const templateData = require('./metadata/metadata');
 const Puppeteer = require('puppeteer');
-const getSlug = require('speakingurl');
 const dayjs = require('dayjs');
 const repoName = require('git-repo-name');
 const username = require('git-username');
@@ -21,7 +20,7 @@ fs.copySync(srcDir + '/assets', outputDir);
 handlebars.registerHelper('markdown', markdownHelper);
 const source = fs.readFileSync(srcDir + '/templates/index.html', 'utf-8');
 const template = handlebars.compile(source);
-const pdfFileName = 'LEEJINKYU_DEVOPS.pdf';
+const pdfFileName = 'LEEJINKYU.pdf';
 const html = template({
   ...templateData,
   baseUrl: `https://${username()}.github.io/${repoName.sync()}`,
@@ -30,8 +29,21 @@ const html = template({
 });
 fs.writeFileSync(outputDir + '/index.html', html);
 
+handlebars.registerHelper('markdown', markdownHelper);
+const pdfSource = fs.readFileSync(srcDir + '/templates/pdfindex.html', 'utf-8');
+const pdfTemplate = handlebars.compile(pdfSource);
+const pdfHtml = pdfTemplate({
+  ...templateData,
+  baseUrl: `https://${username()}.github.io/${repoName.sync()}`,
+  pdfFileName,
+  updated: dayjs().format('MMMM D, YYYY'),
+});
+fs.writeFileSync(outputDir + '/pdfindex.html', pdfHtml);
+
 buildPdf = async function (inputFile, outputFile) {
-  const browser = await Puppeteer.launch();
+  const browser = await Puppeteer.launch({
+    executablePath: '/opt/homebrew/bin/chromium' }
+      );
   const page = await browser.newPage();
   await page.goto(`file://${inputFile}`, {
     waitUntil: 'networkidle0'
@@ -51,4 +63,4 @@ buildPdf = async function (inputFile, outputFile) {
 };
 
 // Build PDF
-buildPdf(`${outputDir}/index.html`, `${outputDir}/${pdfFileName}`);
+buildPdf(`${outputDir}/pdfindex.html`, `${outputDir}/${pdfFileName}`);
--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -4,7 +4,7 @@ const pdf = require('html-pdf');
 module.exports = function buildPdf(input, output) {
   const html = fs.readFileSync(input, 'utf8');
   const options = {
-    format: 'Letter',
+    format: 'A4',
     orientation: 'portrait',
     border: '2.54cm',
     base: 'file://Users/jinkyulee/resume/src/assets/'
